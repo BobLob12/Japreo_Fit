4a1ad6d9756ef78ccaa9bfad6b16e210d5b2136f
BobLob12 <166982455+BobLob12@users.noreply.github.com>
2024-06-17 12:07:46 +0300

    Delete App directory

--- App/AudioCoach/SoundEffects/playsound_player.py
+++
@@ -1,9 +0,0 @@
-from playsound import playsound
-
-
-def UpStage_SoundEffect():
-
- playsound(r'App\AudioCoach\SoundEffects\UpStage.mp3')
-
-if __name__ == '__UpStage_SoundEffect__':
- UpStage_SoundEffect()
