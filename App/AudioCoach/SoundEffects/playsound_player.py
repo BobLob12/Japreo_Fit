7f856abd388a75059c1a7171f2d9bf700e6dd3ed
BobLob12 <166982455+BobLob12@users.noreply.github.com>
2024-06-17 15:49:57 +0300

    Update playsound_player.py

--- App/AudioCoach/SoundEffects/playsound_player.py
+++ App/AudioCoach/SoundEffects/playsound_player.py
@@ -1,9 +1,1 @@
-from playsound import playsound
 
-
-def UpStage_SoundEffect():
-
- playsound(r'App\AudioCoach\SoundEffects\UpStage.mp3')
-
-if __name__ == '__UpStage_SoundEffect__':
- UpStage_SoundEffect()
